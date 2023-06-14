faddaeb0fc9cb428e5f9b2eb83e39dd7095cd415
Denio Nhanale <dnhanale@gmail.com>
2023-06-14 16:00:54 +0200

    Adding remove functionality to remove button

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,19 +1,42 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import Button from './Button';
+import { removeBook } from '../redux/book/booksSlice';
 
-const BookItem = ({ title, author }) => (
-  <div>
-    <li>
-      <h3>
-        {title}
-        ,
-      </h3>
-      <span>
-        {author}
-      </span>
-      <button type="button">Delete</button>
-    </li>
-  </div>
-);
+const BookItem = ({
+  id, title, category, author,
+}) => {
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <li>
+        <h3 id={id}>
+          {title}
+        </h3>
+        <p>
+          {author}
+        </p>
+        <p className="category">
+          {category}
+        </p>
+        <Button title="delete" clickHandler={() => dispatch(removeBook(id))} />
+      </li>
+    </div>
+  );
+};
+
+BookItem.propTypes = {
+  title: PropTypes.string,
+  author: PropTypes.string,
+  category: PropTypes.string,
+  id: PropTypes.string,
+};
+BookItem.defaultProps = {
+  title: null,
+  author: null,
+  category: null,
+  id: null,
+};
 
 export default BookItem;
--- src/redux/book/booksSlice.js
+++ src/redux/book/booksSlice.js
@@ -1,7 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  books: [],
+  books: [
+    {
+      item_id: 'item1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
+  ],
 };
 
 const bookSlice = createSlice({
@@ -12,7 +31,8 @@ const bookSlice = createSlice({
       state.books.push(action.payload);
     },
     removeBook: (state, action) => {
-      state.books.filter((item) => item.id === action.payload.id);
+      const bookId = action.payload;
+      state.books = state.books.filter((item) => item.item_id !== bookId);
     },
   },
 });
