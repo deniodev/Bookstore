883c23a53563a554d51e1664d9b7f89a34232dfa
Denio Nhanale <dnhanale@gmail.com>
2023-06-14 12:16:02 +0200

    Adding Reducers & Set Redux Toolkit

---
+++ src/redux/book/booksSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  books: [],
+};
+
+const bookSlice = createSlice({
+  name: 'book',
+  initialState,
+  reducers: {
+    addBook: (state, action) => {
+      state.books.push(action.payload);
+    },
+    removeBook: (state, action) => {
+      state.books.filter((item) => item.id === action.payload.id);
+    },
+  },
+});
+
+export const { addBook, removeBook } = bookSlice.actions;
+export default bookSlice.reducer;
